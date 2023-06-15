739660aad00abcc5c6d556a6a9cff21cc1407929
dreamogu <dreamogu59@gmail.com>
2023-06-15 15:01:36 +0900

    init : 초기 router 설정 및 레이아웃 css 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+// import Header from './components/Header';
+
+const queryClient = new QueryClient();
+const S = {
+  OutletWrapper: styled.div`
+    max-width: 1264px;
+    width: 100%;
+    background: none;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 auto;
+  `,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* 헤더, 푸터 컴포넌트 생성시 주석처리 해제 및 import 필요. */}
+      {/* <Header /> */}
+      <QueryClientProvider client={queryClient}>
+        <S.OutletWrapper>
+          <Outlet />
+        </S.OutletWrapper>
+      </QueryClientProvider>
+      {/* <Footer /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Login /> },
+      // { index: true, element: <QuestionList /> },
+      // 주석 보고 추가해주세요.
+      { path: 'user/login', element: <Login /> },
+      // { path: 'user/register', element: <컴포넌트명 /> },
+      // { path: 'users/:userId', element: <컴포넌트명 /> },
+
+      // 전체 질문 조회 페이지 예시 /questions?size={size}&page={page}
+      // { path: 'questions', element: <QuestionList /> },
+      // { path: 'questions/register', element: <컴포넌트명 /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
