5fe4ad041be65cf0bd8540a46c4b3c6bb53fc27f
kimyoungsik12 <kys88544@naver.com>
2023-06-15 18:41:06 +0900

    feat : 회원정보 등록,조회,변경

---
+++ server/src/main/java/pre_Project/server/domain/user/dto/UserDto.java
@@ -0,0 +1,61 @@
+package pre_Project.server.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pre_Project.server.domain.user.entitiy.User;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class UserDto {
+    @Getter
+    @AllArgsConstructor
+    public static class Register {
+        @NotBlank
+        @Email
+        private String email;
+        @NotBlank
+        private String userName;
+        @NotBlank
+        private String passWord;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Login {
+        @NotBlank
+        private String email;
+        @NotBlank
+        private String passWord;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Patch {
+        private long userId;
+        private String email;
+        private String userName;
+        private String passWord;
+        private User.UserStatus userStatus;
+
+        public void setUserId(long userId) {
+            this.userId = userId;
+        }
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response {
+        private long userId;
+        private String email;
+        private String userName;
+        private User.UserStatus userStatus;
+        public String getUserStatus(){
+            return userStatus.getStatus();
+        }
+    }
+}
