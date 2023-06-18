0d041ab0286c8b77299ccd251faef9fcfbfbd4e2
dreamogu <dreamogu59@gmail.com>
2023-06-19 00:39:54 +0900

    refactor : 타입 추가

--- client/src/api/loginRequest.ts
+++ client/src/api/loginRequest.ts
@@ -1,8 +1,12 @@
 // 로그인 요청 함수
 
 import axios from 'axios';
+interface LoginInfo {
+  email: string;
+  password: string;
+}
 
-export async function loginRequest(loginInfo) {
+export async function loginRequest(loginInfo: LoginInfo) {
   const response = await axios.post(
     'https://f8f2a07f-23cc-4893-b937-d54fcb607024.mock.pstmn.io/user/login',
     loginInfo
