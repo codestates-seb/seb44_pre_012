8f404e852ec70db488035fa394b1be27dbc2467b
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-06-22 23:31:37 +0900

    feat: 답변 작성 날짜 포맷팅 함수 추가

--- client/src/util/formatElapsedTime.ts
+++ client/src/util/formatElapsedTime.ts
@@ -52,3 +52,67 @@ export function formatElapsedTime(createdAt: string) {
 
   return formattedElapsedTime;
 }
+
+export function formatAnswerElapsedTime(createdAt: string) {
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const tempPostedTime: Date = new Date(createdAt);
+  const now = new Date();
+  const elapsedMilliseconds = now.getTime() - tempPostedTime.getTime();
+  const elapsedSeconds = Math.floor(elapsedMilliseconds / 1000);
+  const elapsedMinutes = Math.floor(elapsedSeconds / 60);
+  const elapsedHours = Math.floor(elapsedMinutes / 60);
+  const elapsedDays = Math.floor(elapsedHours / 24);
+
+  let formattedElapsedTime;
+  switch (true) {
+    case elapsedDays > 365:
+      const formattedYearDate = `${
+        monthNames[tempPostedTime.getMonth()]
+      } ${tempPostedTime.getDate()}, ${tempPostedTime.getFullYear()}`;
+      const formattedYearTime = `${tempPostedTime.getHours()}:${String(
+        tempPostedTime.getMinutes()
+      ).padStart(2, '0')}`;
+      formattedElapsedTime = `${formattedYearDate}`;
+      break;
+    case elapsedDays > 0:
+      const formattedDate = `${
+        monthNames[tempPostedTime.getMonth()]
+      } ${tempPostedTime.getDate()}`;
+      const formattedTime = `${tempPostedTime.getHours()}:${String(
+        tempPostedTime.getMinutes()
+      ).padStart(2, '0')}`;
+      formattedElapsedTime = `${formattedDate} at ${formattedTime}`;
+      break;
+    case elapsedHours > 1:
+      formattedElapsedTime = `${elapsedHours} hours ago`;
+      break;
+    case elapsedHours === 1:
+      formattedElapsedTime = `${elapsedHours} hour ago`;
+      break;
+    case elapsedMinutes > 1:
+      formattedElapsedTime = `${elapsedMinutes} minutes ago`;
+      break;
+    case elapsedMinutes === 1:
+      formattedElapsedTime = `${elapsedMinutes} minute ago`;
+      break;
+    default:
+      formattedElapsedTime = `${elapsedSeconds} seconds ago`;
+      break;
+  }
+
+  return formattedElapsedTime;
+}
