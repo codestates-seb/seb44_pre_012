40fe6f87ab3899b12244acc96666b1f343d7f4a7
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-06-22 13:39:31 +0900

    feat: 질문 목록 데이터 localStorage 저장

--- client/src/temp/handlers.ts
+++ client/src/temp/handlers.ts
@@ -1,8 +1,9 @@
-// 목업 서버
+// 목업 서버 (나중에 쉽게 삭제하게 한 파일에 다 넣어두면 좋을 것 같아요)
 import allQuestions from './AllQuestionQuery.json';
 import { rest } from 'msw';
 
 export const handlers = [
+  // allQuestion 쿼리
   rest.get('/questions', (req, res, ctx) => {
     const searchParams = new URLSearchParams(req.url.search);
     const size = searchParams.get('size');
@@ -27,5 +28,21 @@ export const handlers = [
       ctx.status(200),
       ctx.json({ data: processedData, pageInfo: pageInfo })
     );
+  }),
+
+  //답변 POST
+  rest.post('/answers/register/:questionId', (req, res, ctx) => {
+    const { questionId } = req.params;
+    const { answerContent, createdAt, userInfo } = req.body;
+
+    const newAnswer = {
+      answerId
+      answerContent,
+      createdAt,
+      userInfo,
+    };
+
+    // 새로운 답변을 반환합니다.
+    return res(ctx.status(200), ctx.json(newAnswer));
   }),
 ];
