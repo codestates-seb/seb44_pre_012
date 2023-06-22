d3dd1d323328bbd7d81432b3cfed44233b1d1388
dreamogu <dreamogu59@gmail.com>
2023-06-23 00:20:45 +0900

    feat : 로그인 상태 분기 처리

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,134 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
 import { MdMenu, MdAccountBox, MdHelp } from 'react-icons/md';
-import { AiOutlineClose, AiOutlineSearch } from 'react-icons/ai'
-import { BsInboxFill } from 'react-icons/bs'
-import { GiDiamondTrophy } from 'react-icons/gi'
-import { FaStackExchange } from 'react-icons/fa'
-import { useState } from "react";
+import { AiOutlineClose, AiOutlineSearch } from 'react-icons/ai';
+import { BsInboxFill } from 'react-icons/bs';
+import { GiDiamondTrophy } from 'react-icons/gi';
+import { FaStackExchange } from 'react-icons/fa';
+import { useState } from 'react';
 import logoIcon from '../assets/headerlogo.png';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+
+export default function Header() {
+  const isLoggedIn = useSelector(
+    (state: RootState) => state.auth.login.isLogin
+  );
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDown, setIsDown] = useState(false);
+  const searchData = [
+    { keyword: '[tag]', description: 'search within a tag' },
+    { keyword: 'user: 1234', description: 'search by author' },
+    { keyword: '"words here"', description: 'exact phrase' },
+    { keyword: 'collective: "Name"', description: 'collective content' },
+    { keyword: 'answers: 0', description: 'unanswered questions' },
+    { keyword: 'score: 3', description: 'posts with a 3+ score' },
+    { keyword: 'is: question', description: 'type of post' },
+    { keyword: 'isaccepted yes', description: 'search within status' },
+  ];
+  const topButton = [
+    { icon: <BsInboxFill />, title: 'Recent inbox messages' },
+    { icon: <GiDiamondTrophy />, title: 'Help Center and other resources' },
+    { icon: <MdHelp />, title: 'Help Center and other resources' },
+  ];
+
+  const MenuDropdown = () => {
+    setIsOpen(!isOpen);
+    console.log('작동한다');
+  };
+
+  const SearchInputDropdown = () => {
+    setIsDown(!isDown);
+    console.log('작동한다1');
+  };
+
+  return (
+    <S.TopbarWrapper>
+      <S.TopbarContainer>
+        {!isLoggedIn && (
+          <S.TopbarMenuBtn onClick={MenuDropdown}>
+            {isOpen ? <AiOutlineClose /> : <MdMenu />}
+          </S.TopbarMenuBtn>
+        )}
+        <S.TopbarLogoImgContainer>
+          <Link to="/">
+            <S.TopbarLogoImg src={logoIcon} />
+          </Link>
+        </S.TopbarLogoImgContainer>
+        <S.TopbarNav>
+          <S.NavButton>About</S.NavButton>
+          <S.NavButton>Produts</S.NavButton>
+          <S.NavButton>For Teams</S.NavButton>
+        </S.TopbarNav>
+        <S.TopForm>
+          <S.TopFormContainer onClick={SearchInputDropdown} isdown={isDown}>
+            <AiOutlineSearch />
+            <S.TopFormInput type="text" placeholder="Search..." />
+          </S.TopFormContainer>
+          <S.TopSearchPopoverContainer isdown={isDown}>
+            <S.TopSearchPopoverWrapper>
+              {searchData.map((el, idx) => (
+                <S.TopSearchPopoverContent key={idx}>
+                  <S.TopSearchPopoverContentTitle>
+                    {el.keyword}
+                  </S.TopSearchPopoverContentTitle>
+                  <S.TopSearchPopoverContentMSG>
+                    {el.description}
+                  </S.TopSearchPopoverContentMSG>
+                </S.TopSearchPopoverContent>
+              ))}
+            </S.TopSearchPopoverWrapper>
+            <S.TopSearchPopoverBottom>
+              <S.TopSearchPopoverBottomWrapper>
+                <S.TopSearchPopoverQuestionBtn>
+                  Ask a question
+                </S.TopSearchPopoverQuestionBtn>
+                <S.TopSearchPopoverHelpBtn>
+                  Serch help
+                </S.TopSearchPopoverHelpBtn>
+              </S.TopSearchPopoverBottomWrapper>
+            </S.TopSearchPopoverBottom>
+          </S.TopSearchPopoverContainer>
+        </S.TopForm>
+        <S.TopbarBtns>
+          {!isLoggedIn ? (
+            <>
+              <Link to="users/login">
+                <S.TopbarLoginBtn>Log in</S.TopbarLoginBtn>
+              </Link>
+              <Link to="users/register">
+                <S.TopbarSignupBtn>Sign up</S.TopbarSignupBtn>
+              </Link>
+            </>
+          ) : (
+            <>
+              <S.TopbarBtn role="menuitem" title="User Name">
+                <MdAccountBox />
+                <S.TopbarReputation>
+                  <div>1</div>
+                </S.TopbarReputation>
+              </S.TopbarBtn>
+              {topButton.map((el, idx) => (
+                <S.TopbarBtn key={idx} title={el.title}>
+                  {el.icon}
+                </S.TopbarBtn>
+              ))}
+              <Link to="users/logout">
+                <S.TopbarBtn
+                  role="menuitem"
+                  title="A list of all 181 Stack Exchange sites"
+                >
+                  <FaStackExchange />
+                </S.TopbarBtn>
+              </Link>
+            </>
+          )}
+        </S.TopbarBtns>
+      </S.TopbarContainer>
+    </S.TopbarWrapper>
+  );
+}
 
 const S = {
   TopbarWrapper: styled.header`
@@ -55,7 +177,7 @@ const S = {
     list-style: none;
   `,
   NavButton: styled.li`
-    color: 	#525960;
+    color: #525960;
     padding: 8px;
     margin: 2px;
     border-radius: 30px;
@@ -70,11 +192,13 @@ const S = {
     flex-grow: 1;
     padding: 0px 3px 0px 3px;
   `,
-  TopFormContainer: styled.div<{ isdown:boolean }>`
+  TopFormContainer: styled.div<{ isdown: boolean }>`
     display: flex;
     align-items: center;
-    border: 1px solid ${(props)=>(props.isdown ? "#69b9f7" : "var(--color-ui-border)")};
-    box-shadow: ${(props) => (props.isdown ? "0px 0px 0px 4px rgba(105, 185, 247, 0.3)" : "none")};
+    border: 1px solid
+      ${props => (props.isdown ? '#69b9f7' : 'var(--color-ui-border)')};
+    box-shadow: ${props =>
+      props.isdown ? '0px 0px 0px 4px rgba(105, 185, 247, 0.3)' : 'none'};
     border-radius: 3px;
     padding-left: 5px;
   `,
@@ -86,26 +210,26 @@ const S = {
     padding-left: 5px;
     outline: none;
   `,
-  TopSearchPopoverContainer: styled.div<{ isdown:boolean }>`
+  TopSearchPopoverContainer: styled.div<{ isdown: boolean }>`
     background-color: white;
     border: 1px solid var(--color-ui-border);
-    box-shadow:0px 1px 3px 3px var(--color-ui-border-shadow);
+    box-shadow: 0px 1px 3px 3px var(--color-ui-border-shadow);
     border-radius: 3px;
     display: flex;
     flex-direction: column;
     position: relative;
     margin-top: 10px;
     filter: drop-shadow(0px 0px 5px var(--color-ui-border));
-    display: ${(props) => (props.isdown ? "block" : "none")};
+    display: ${props => (props.isdown ? 'block' : 'none')};
     &::before {
-      content: "";
+      content: '';
       position: absolute;
       top: -8px;
       left: 50%;
       transform: translateX(-50%);
-	    border-bottom: 12px solid white;/* 화살표 */
-	    border-left: 12px solid transparent;
-	    border-right: 12px solid transparent;
+      border-bottom: 12px solid white; /* 화살표 */
+      border-left: 12px solid transparent;
+      border-right: 12px solid transparent;
     }
   `,
   TopSearchPopoverWrapper: styled.div`
@@ -123,7 +247,7 @@ const S = {
   TopSearchPopoverContentTitle: styled.span`
     font-size: 13px;
     color: black;
-    margin-right: 3px;      
+    margin-right: 3px;
   `,
   TopSearchPopoverContentMSG: styled.span`
     font-size: 13px;
@@ -180,193 +304,87 @@ const S = {
     }
   `,
   TopbarSignupBtn: styled.li`
-      height: 30px;
-      font-size: 13px;
-      margin-right: 3px;
-      cursor: pointer;
-      display: flex;
-      align-items: center;
-      border-radius: 3px;
-      padding: 0px 10px;
-      background-color: var(--color-button-blue);
-      border: 1px solid var(--color-button-border);
-      color: var(--color--button-signup);
-      &:hover {
-        background-color: var(--color-button-blue-hover);
-      }
+    height: 30px;
+    font-size: 13px;
+    margin-right: 3px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    border-radius: 3px;
+    padding: 0px 10px;
+    background-color: var(--color-button-blue);
+    border: 1px solid var(--color-button-border);
+    color: var(--color--button-signup);
+    &:hover {
+      background-color: var(--color-button-blue-hover);
+    }
   `,
   TopbarBtn: styled.li`
-      width: 40px;
-      height: 49px;
-      font-size: 30px;
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      &:hover {
-        background-color: var(--color-navbar-button-hover);
-      }
+    width: 40px;
+    height: 49px;
+    font-size: 30px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      background-color: var(--color-navbar-button-hover);
+    }
   `,
   TopbarReputation: styled.div`
-      width: 6px;
-      height: 12px;
-      font-size: 12px;
-      display: flex;
-      align-items: center;
+    width: 6px;
+    height: 12px;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
   `,
   TopbarInboxBtn: styled.li`
-      width: 40px;
-      height: 49px;
-      font-size: 30px;
-      margin-left: 12px;
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      &:hover {
-        background-color: var(--color-navbar-button-hover);
-      }
+    width: 40px;
+    height: 49px;
+    font-size: 30px;
+    margin-left: 12px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      background-color: var(--color-navbar-button-hover);
+    }
   `,
   TopbarAchievementsBtn: styled.li`
-      width: 40px;
-      height: 49px;
-      font-size: 30px;
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      &:hover {
-        background-color: var(--color-navbar-button-hover);
-      }
+    width: 40px;
+    height: 49px;
+    font-size: 30px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      background-color: var(--color-navbar-button-hover);
+    }
   `,
   TopbarHelpBtn: styled.li`
-      width: 40px;
-      height: 49px;
-      font-size: 30px;
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      &:hover {
-        background-color: var(--color-navbar-button-hover);
-      }
+    width: 40px;
+    height: 49px;
+    font-size: 30px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      background-color: var(--color-navbar-button-hover);
+    }
   `,
   TopbarExchangeBtn: styled.li`
-      width: 40px;
-      height: 49px;
-      font-size: 27px;
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      &:hover {
-        background-color: var(--color-navbar-button-hover);
-      }
+    width: 40px;
+    height: 49px;
+    font-size: 27px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      background-color: var(--color-navbar-button-hover);
+    }
   `,
 };
-
-export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isDown, setIsDown] = useState(false);
-  const searchData = [
-    { keyword: '[tag]', description: 'search within a tag' },
-    { keyword: 'user: 1234', description: 'search by author' },
-    { keyword: '"words here"', description: 'exact phrase' },
-    { keyword: 'collective: "Name"', description: 'collective content' },
-    { keyword: 'answers: 0', description: 'unanswered questions' },
-    { keyword: 'score: 3', description: 'posts with a 3+ score' },
-    { keyword: 'is: question', description: 'type of post' },
-    { keyword: 'isaccepted yes', description: 'search within status' }
-  ]
-  const topButton = [
-    { icon: <BsInboxFill/>, title: "Recent inbox messages" },
-    { icon: <GiDiamondTrophy/>, title: "Help Center and other resources" },
-    { icon: <MdHelp/>, title: "Help Center and other resources" }
-  ]
-
-  const MenuDropdown = () => {
-    setIsOpen(!isOpen)
-    console.log("작동한다")
-  }
-
-  const SearchInputDropdown = () => {
-    setIsDown(!isDown)
-    console.log("작동한다1")
-  }
-
-  return (
-  <S.TopbarWrapper>
-    <S.TopbarContainer>
-      <S.TopbarMenuBtn onClick={MenuDropdown}>      
-        {isOpen ? <AiOutlineClose /> : <MdMenu />}
-      </S.TopbarMenuBtn>
-      <S.TopbarLogoImgContainer>
-          <Link to="/">
-            <S.TopbarLogoImg src={logoIcon}/>
-          </Link>
-        </S.TopbarLogoImgContainer>
-      <S.TopbarNav>
-        <S.NavButton>About</S.NavButton>
-        <S.NavButton>Produts</S.NavButton>
-        <S.NavButton>For Teams</S.NavButton>
-      </S.TopbarNav>
-      <S.TopForm>
-        <S.TopFormContainer 
-          onClick={SearchInputDropdown}
-          isdown={isDown}>
-          <AiOutlineSearch />
-          <S.TopFormInput 
-            type="text"
-            placeholder="Search..."/>
-        </S.TopFormContainer>
-        <S.TopSearchPopoverContainer isdown={isDown}>
-          <S.TopSearchPopoverWrapper>
-            {searchData.map((el, idx) => (
-              <S.TopSearchPopoverContent key={idx}>
-                <S.TopSearchPopoverContentTitle>{el.keyword}</S.TopSearchPopoverContentTitle>
-                <S.TopSearchPopoverContentMSG>{el.description}</S.TopSearchPopoverContentMSG>
-                </S.TopSearchPopoverContent>
-              ))}
-          </S.TopSearchPopoverWrapper>
-          <S.TopSearchPopoverBottom>
-            <S.TopSearchPopoverBottomWrapper>
-              <S.TopSearchPopoverQuestionBtn>
-                Ask a question
-              </S.TopSearchPopoverQuestionBtn>
-              <S.TopSearchPopoverHelpBtn>
-                Serch help
-              </S.TopSearchPopoverHelpBtn>
-            </S.TopSearchPopoverBottomWrapper>
-          </S.TopSearchPopoverBottom>
-        </S.TopSearchPopoverContainer>
-      </S.TopForm>
-      <S.TopbarBtns>
-        <Link to="users/login">
-          <S.TopbarLoginBtn>Log in</S.TopbarLoginBtn>
-        </Link>
-        <S.TopbarSignupBtn>Sign up</S.TopbarSignupBtn>
-        <S.TopbarBtn role="menuitem" title="User Name">
-          <MdAccountBox/>
-          <S.TopbarReputation>
-            <div>1</div>
-          </S.TopbarReputation>
-        </S.TopbarBtn>
-        {topButton.map((el, idx) => 
-        <S.TopbarBtn key={idx} title={el.title}>{el.icon}
-        </S.TopbarBtn>
-        )}
-        <Link to="users/logout">
-          <S.TopbarBtn role="menuitem" title="A list of all 181 Stack Exchange sites">
-            <FaStackExchange/>
-          </S.TopbarBtn>
-        </Link>
-      </S.TopbarBtns>
-    </S.TopbarContainer>
-  </S.TopbarWrapper>
-  ) 
-}
-
-
-
-
-
